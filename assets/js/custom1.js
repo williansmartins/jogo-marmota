024d99806624c6ca81751a9d18f8b4b86404c01a
wsms <wsms@gft.com>
2018-08-02 22:41:39 -0300

    comite atomico - gacp

--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,1 +1,10 @@
-console.log('script from cutsom1');
+$( document ).ready(function() {
+
+	var casinha = `<div class="casinha">
+		<img class="imagem imagem1" src="https://st2.depositphotos.com/7857468/12331/v/950/depositphotos_123313186-stock-illustration-cartoon-funny-groundhog.jpg" width="100" /> 
+		<img class="imagem imagem2" src="https://png.pngtree.com/element_origin_min_pic/16/09/04/0057caf8198cf76.jpg" width="100" />
+	</div>`;
+
+	$(".tabuleiro").append(casinha);
+	alert(123);
+});
