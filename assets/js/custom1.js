0e298ad10915187ce6ee882a58d037386c841ab3
Willians Martins de Morais <wsms@gft.com>
2018-08-03 17:41:38 -0300

    add animacao

--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,14 +1,30 @@
+var quantidade = 15;
+var tempo = 1000;
+
 $( document ).ready(function() {
 
-	var quantidade = 15;
 
 	for (var i = 1; i <=quantidade; i++) {
 		$(".tabuleiro").append('<div class="casinha" id="'+i+'">' +
-			'<img class="imagem imagem1" src="images/marmota2.jpg" width="100" /> ' +
-			'<img class="imagem imagem2" src="images/marmota1.jpg" width="100" />' +
+			'<img class="imagem imagem1 visibility-hidden" src="images/marmota2.jpg" width="100" /> ' +
+			'<img class="imagem imagem2 visibility-hidden" src="images/marmota1.jpg" width="100" />' +
 		'</div>');
 	}
+	
+	//$(".tabuleiro .casinha .imagem2").removeClass('hide');
 
-	$(".tabuleiro").css({cursor : 'url(../images/marreta.jpg), auto'});
+	$(".tabuleiro .casinha .imagem").css('visibility', 'hidden');
 
+	var myVar = setInterval(apresentar, tempo);
 });
+
+function apresentar() {
+	var numeroRandomico = Math.floor(Math.random() * quantidade) + 1;
+  	$(".tabuleiro .casinha .imagem").css('visibility', 'hidden');
+  	$(".tabuleiro .casinha#"+numeroRandomico+" .imagem1").css('visibility', 'visible');
+  	console.info("numeroRandomico" + numeroRandomico);
+}
+ 
+function stopColor() {
+  clearInterval(myVar);
+}
